c8dc542f65cb7a314a7cfe9234ddcb195c55e3c7
Subhajit Bag <117541285+subhajitb7@users.noreply.github.com>
2023-09-21 20:35:43 +0000

    modified:   1_basics/01_variables.js

--- 1_basics/01_variables.js
+++ 1_basics/01_variables.js
@@ -4,4 +4,15 @@ var accountPassword = "@#$34343"
 accountCity = "Kolkata"
 let accountState;
 
+console.log(accountId)
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState ])
+
+// accountId = 19005 / not allowed
+
+accountEmail = "subhaiitb@gmail.com"
+accountPassword = "#&*83hde"
+accountCity = "Bengaluru"
+accountState = "Karnataka"
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState ])
+
+/* Prefer not to use var because of issues in block and functioanal scope */
