c0323b06e683bab0a7d98144d0f74255266c0d8e
Subhajit Bag <117541285+subhajitb7@users.noreply.github.com>
2023-09-21 21:49:40 +0000

    new file:   1_basics/02_datatypes.js

---
+++ 1_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all js code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+let name = "Subhajit"
+let age = 20
+let isLoggedin = true
+let state;
+
+//string => ""
+//number => 2 to power 53
+//array 
+//object
+//boolean => true/false
+//undefined => value is not defined
+//null => standalone value
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
