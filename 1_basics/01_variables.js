ed5608d3cd197127e62ed63ba7ba942ac3c95072
Subhajit Bag <117541285+subhajitb7@users.noreply.github.com>
2023-09-21 20:00:56 +0000

    added

---
+++ 1_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 19003
+let accountEmail = "subhajit@gmail.com"
+var accountPassword = "@#$34343"
+accountCity = "Kolkata"
+let accountState;
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState ])
