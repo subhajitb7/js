6e1cc79fa011f71a248bf43d2ade4fe6af52c775
Subhajit Bag <117541285+subhajitb7@users.noreply.github.com>
2023-09-22 07:45:03 +0000

    update 1_basics/01_variables.js

--- 1_basics/01_variables.js
+++ 1_basics/01_variables.js
@@ -12,7 +12,7 @@ console.table([accountId, accountEmail, accountPassword, accountCity, accountSta
 accountEmail = "subhaiitb@gmail.com"
 accountPassword = "#&*83hde"
 accountCity = "Bengaluru"
-accountState = "Karnataka"
+accountState = "Savings"
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState ])
 
 /* Prefer not to use var because of issues in block and functioanal scope */
