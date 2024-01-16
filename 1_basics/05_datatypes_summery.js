6226b59639d0efe8002bdec3a2cecfc464ad5b0e
Subhajit Bag <117541285+subhajitb7@users.noreply.github.com>
2024-01-16 17:15:31 +0000

    add 05_datatypes_summery.js

---
+++ 1_basics/05_datatypes_summery.js
@@ -0,0 +1,67 @@
+//Datatypes in JS
+
+//JS is a dynamically typed language
+//This means that the same variable can be used to hold different data types
+
+/*
+#Primitive
+    String, Number, Boolean, BigInt, Symbol, null, undefined
+*/
+
+//String
+let fName = "Subhajit"
+let age = "22"
+//console.log(typeof fName)
+//console.log(typeof age)
+
+//Number
+const score = 100
+const scoreValue = 99.7
+//console.log(typeof score)
+//console.log(typeof scoreValue)
+
+//Boolean
+let isLoggedIn = true
+//console.log(typeof isLoggedIn)
+
+//BigInt
+const bigNumber = 37463762370654747043792500357
+//console.log(typeof isLoggedIn)
+
+//Symbol
+let id = Symbol('123')
+let anotherId = Symbol('123')
+//console.log(id == anotherId)
+//console.log(id === anotherId)
+
+//null
+let outSideTemp = null
+//console.log(typeof outSideTemp)
+
+//undefined
+let userEmail
+//console.log(typeof userEmail)
+
+
+
+/* 
+#Refernce (Non-Primitive)
+    Array, Objects, Function
+*/
+
+//Array
+const success = ["Habit", "Consistency", "Discipine"]
+//console.log(typeof success)
+
+//Object
+let myobj = {
+    name: "Subhajit",
+    age: 22,
+}
+//console.log(typeof myobj)
+
+//Function
+const myfunc = function (){
+    console.log("Hello World")
+}
+console.log(typeof myfunc)
