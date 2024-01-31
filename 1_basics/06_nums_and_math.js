9c52923bdeaa8f440321dc6bc825a2a1f534c394
Subhajit Bag <117541285+subhajitb7@users.noreply.github.com>
2024-01-31 17:20:54 +0000

    modified:   1_basics/06_nums_and_math.js

--- 1_basics/06_nums_and_math.js
+++ 1_basics/06_nums_and_math.js
@@ -5,13 +5,15 @@ const balance = new Number(100)
 // console.log(balance)
 
 // console.log(balance.toString().length);
-// console.log(balance.toFixed(1));
+// console.log(balance.toFixed(2));
 
 const otherNumber = 123.8966
 
-// console.log(otherNumber.toPrecision(4));
+//  console.log(otherNumber.toPrecision(4));
+//  console.log(otherNumber.toPrecision(3));
 
 const hundreds = 1000000
+// console.log(hundreds.toLocaleString());
 // console.log(hundreds.toLocaleString('en-IN'));
 
 // +++++++++++++ Maths +++++++++++++++++++++++++++++
@@ -24,11 +26,17 @@ const hundreds = 1000000
 // console.log(Math.min(4, 3, 6, 8));
 // console.log(Math.max(4, 3, 6, 8));
 
-console.log(Math.random());
-console.log((Math.random()*10) + 1);
-console.log(Math.floor(Math.random()*10) + 1);
+// console.log(Math.random());
+// console.log(Math.random()*10); 
+// console.log((Math.random()*10) + 1); // +1 to avoid 0 value
+// console.log(Math.floor(Math.random()*10) + 1);
 
 const min = 10
 const max = 20
 
-console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+console.log(Math.floor(Math.random() * (max - min)))
+console.log(Math.floor(Math.random() * (max - min)) + min) 
+// The maximum is exclusive and the minimum is inclusive
+console.log(Math.floor(Math.random() * (max - min + 1)) + min) 
+// The maximum is inclusive and the minimum is inclusive
+
